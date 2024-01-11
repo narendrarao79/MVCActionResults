b2a44f6e34cd538e7944b8b860cad9844e0e548b
narendrarao79 <narendrayadavbdn2018@gmail.com>
2024-01-11 20:25:50 +0530

    Add project files.

---
+++ MVCActionResults/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCActionResults.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Register()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult ForgotPassword()
+        {
+            return View("ForgotPwd");
+        }
+        public ViewResult ResetPassword()
+        {
+            return View("~/Views/Home/ResetPwd.cshtml");
+        }
+        public ViewResult Contact()
+        {
+            return View();
+        }
+        public ViewResult Mission()
+        {
+            return View("~/Views/Test/Mission.cshtml");
+        }
+        public ViewResult About()
+        {
+            return View("~/Views/Home/About.cshtml");
+        }
+        public ViewResult Show(int id)
+        {
+            if (id == 1)
+                return View("Show1");
+            else
+                return View("Show2");
+        }
+    }
+}
